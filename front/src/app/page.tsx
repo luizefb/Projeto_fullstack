f374ec70abd144537194ade938057066c194aa87
Luiz <hotemaki@gmail.com>
2025-03-24 20:52:18 -0300

    Estilização simples

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -55,51 +55,51 @@ export default function Home() {
 
   function renderizarFormProduto() {
     return (
-      <div className="flex gap-5 items-end">
+      <div className="flex flex-col gap-5 items-center">
         <div className="flex flex-col">
-          <label htmlFor="nome">Nome produto</label>
+          <label className="font-bold" htmlFor="nome">Nome produto</label>
           <input 
             id="nome"
             type='text' 
             value={produto.nome ?? ''} 
             onChange={e => setProduto({ ...produto, nome: e.target.value })}
-            className="bg-gray-300 border-2 rounded-md p-2"
+            className="bg-gray-100 border-2 rounded-md p-2"
             />
         </div>
 
         <div className="flex flex-col">
-          <label htmlFor="descricao">Descrição</label>
+          <label className="font-bold" htmlFor="descricao">Descrição</label>
           <input 
             id="descricao"
             type='text' 
             value={produto.descricao ?? ''} 
             onChange={e => setProduto({ ...produto, descricao: e.target.value })}
-            className="bg-gray-300 border-2 rounded-md p-2"
+            className="bg-gray-100 border-2 rounded-md p-2"
             />
         </div>
 
         <div className="flex flex-col">
-          <label htmlFor="preco">Preço</label>
+          <label className="font-bold" htmlFor="preco">Preço</label>
             <input 
               id="preco"
               type='number' 
               value={produto.preco ?? ''} 
               onChange={e => setProduto({ ...produto, preco: +e.target.value })}
-              className="bg-gray-300 border-2 rounded-md p-2"
+              className="bg-gray-100 border-2 rounded-md p-2"
               />
         </div>
         <div>
           {produto.id ? (
               <button 
               onClick={alterarProdutos}
-              className="bg-blue-500 rounded-md px-4 py-2"
+              className="bg-blue-500 rounded-md px-4 py-2 font-bold"
             >
               Alterar Produto
             </button>
           ) : (
             <button 
             onClick={criarProdutos}
-            className="bg-blue-500 rounded-md px-4 py-2"
+            className="bg-blue-500 rounded-md px-4 py-2 font-bold"
           >
             Criar Produto
           </button>
@@ -113,8 +113,8 @@ export default function Home() {
     return (
       <div className="flex flex-col gap-2">
         {produtos.map((produto: any) =>(
-          <div key={produto.id} className="flex px-4 py-2 items-center gap-2 bg-cyan-200 p-2 rounded-md">
-            <div className="flex-1 ">{produto.nome}</div>
+          <div key={produto.id} className="flex px-4 py-2 items-center gap-2 bg-gray-100 p-2 rounded-md">
+            <div className="flex-1 font-bold">{produto.nome}</div>
             <div>{produto.preco}</div>
 
             <div>
@@ -139,9 +139,14 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-10">
-      {renderizarFormProduto()}
-      {renderizarProdutos()}
+    <div className="flex justify-center items-center h-screen gap-30">
+      <div className="bg-blue-400 py-6 px-8 rounded-xl">
+        {renderizarFormProduto()}
+      </div>
+      
+      <div className="bg-blue-400 py-6 px-8 rounded-xl">
+       {renderizarProdutos()}
+      </div>
     </div>
   );
 }
