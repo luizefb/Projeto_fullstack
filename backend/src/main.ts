bdea8953a9f14717deeeb0e08e3717b6610a44c0
Luiz <hotemaki@gmail.com>
2025-03-24 22:31:32 -0300

    Vercel backend update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true });
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
